93afd3414e54fa235db33fda3c45dc32ec9ad9dc
_Ghosteye <yqcyecong@163.com>
2025-05-16 16:07:02 +0800

    fix(button): add `asChild` prop definition to prevent runtime warning

--- client/src/components/ui/button/Button.vue
+++ client/src/components/ui/button/Button.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
-import { cn } from '~/lib/utils'
 import { Primitive, type PrimitiveProps } from 'reka-ui'
+import { cn } from '~/lib/utils'
 import { type ButtonVariants, buttonVariants } from '.'
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  as?: string
+  asChild?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
